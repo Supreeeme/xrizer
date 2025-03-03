ea0ec6dd0367d547ecde67fadd38cf85be65c922
knah <4705494+knah@users.noreply.github.com>
2025-03-03 23:34:45 +0100

    Formatting and style improvements

--- src/input/profiles/knuckles.rs
+++ src/input/profiles/knuckles.rs
@@ -42,7 +42,8 @@ impl InteractionProfile for Knuckles {
                 to: "squeeze/value",
                 stop: true,
             },
-            PathTranslation { // button bindings
+            PathTranslation {
+                // button bindings
                 from: "squeeze/touch",
                 to: "squeeze/value",
                 stop: true,
