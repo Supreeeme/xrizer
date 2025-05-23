63e897dadf2ba9479ca15a81a2ab3d145e3d99a1
Kitlith <kitlith@kitl.pw>
2025-05-23 13:17:53 -0700

    Output library to platform directory based on target info
    The approach taken here is to read target_{os,arch} in build.rs,
    determine the correct platform directory and filename (minus the
    file extension), and then expose that information in environment
    variables. These environment variables can then be read in the
    xbuild wrapper script, and used to place things in the correct spot.

--- build.rs
+++ build.rs
@@ -1,6 +1,33 @@
+use std::env;
+
 fn main() {
-    let out_dir = std::env::var("OUT_DIR").unwrap();
+    let out_dir = env::var("OUT_DIR").unwrap();
     for path in shaders::compile(&out_dir) {
         println!("cargo::rerun-if-changed={}", path.to_str().unwrap());
     }
+
+    let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
+    let target_os = target_os.as_str();
+
+    let target_arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
+    let target_arch = target_arch.as_str();
+
+    let vrclient_name = match (target_os, target_arch) {
+        ("windows", "x86_64") => "vrclient_x64",
+        _ => "vrclient",
+    };
+
+    let platform_location = match (target_os, target_arch) {
+        ("windows", _) => "bin/",
+        ("linux", "x86") => "bin/",
+        ("linux", "x86_64") => "bin/linux64/",
+        ("linux", "aarch64") => "bin/linuxarm64/",
+        ("android", "arm") => "bin/androidarm32/",
+        ("android", "aarch64") => "bin/androidarm64/",
+        ("macos", _) => "bin/osx32/",
+        _ => "bin/unknown_plaform/",
+    };
+
+    println!("cargo::rustc-env=XRIZER_OPENVR_PLATFORM_DIR={platform_location}");
+    println!("cargo::rustc-env=XRIZER_OPENVR_VRCLIENT_NAME={vrclient_name}");
 }
