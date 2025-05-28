8eeea9449d3ee9a8e7600c2c19ae095561a58bb6
Kitlith <kitlith@kitl.pw>
2025-05-27 19:46:39 -0700

    fixup! apply feedback

--- build.rs
+++ build.rs
@@ -12,21 +12,25 @@ fn main() {
     let target_arch = env::var("CARGO_CFG_TARGET_ARCH").unwrap();
     let target_arch = target_arch.as_str();
 
+    // Object name and platform directory logic is generally based on a couple of pieces of openvr_api code:
+    //  - platform directory names: https://github.com/ValveSoftware/openvr/blob/ae46a8dd0172580648c8922658a100439115d3eb/src/vrcore/pathtools_public.h#L127-L157
+    //  - general logic and special cases: https://github.com/ValveSoftware/openvr/blob/ae46a8dd0172580648c8922658a100439115d3eb/src/openvr_api_public.cpp#L128-L144
+    // The android and macos platforms have been omitted, since we are currently uninterested in supporting them.
+
     let vrclient_name = match (target_os, target_arch) {
         ("windows", "x86_64") => "vrclient_x64",
         _ => "vrclient",
     };
 
     let platform_location = match (target_os, target_arch) {
-        ("windows", _) => "bin/",
+        ("windows", "x86") | ("windows", "x86_64") => "bin/",
         ("linux", "x86") => "bin/",
         ("linux", "x86_64") => "bin/linux64/",
         ("linux", "aarch64") => "bin/linuxarm64/",
-        ("android", "arm") => "bin/androidarm32/",
-        ("android", "aarch64") => "bin/androidarm64/",
-        ("macos", _) => "bin/osx32/",
-        _ => "bin/unknown_plaform/",
+        _ => panic!("Unsupported architecture."),
     };
+
+    panic!("test");
 
     println!("cargo::rustc-env=XRIZER_OPENVR_PLATFORM_DIR={platform_location}");
     println!("cargo::rustc-env=XRIZER_OPENVR_VRCLIENT_NAME={vrclient_name}");
