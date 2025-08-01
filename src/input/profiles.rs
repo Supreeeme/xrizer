a67e92ec2f4dcc343e6b960408873ae2e9be98df
hare_ware <3469405-hare_ware@users.noreply.gitlab.com>
2025-08-01 19:44:56 -0400

    Set profile path for HP Reverb G2 controllers

--- src/input/profiles.rs
+++ src/input/profiles.rs
@@ -1,6 +1,6 @@
+pub mod hp_motion_controller;
 pub mod knuckles;
 pub mod ms_motion_controller;
-pub mod hp_motion_controller;
 pub mod oculus_touch;
 pub mod samsung_odyssey_controller;
 pub mod simple_controller;
@@ -13,9 +13,9 @@ use crate::{
     input::profiles::samsung_odyssey_controller::SamsungOdysseyController, openxr_data::Hand,
 };
 use glam::Mat4;
+use hp_motion_controller::ReverbG2Controller;
 use knuckles::Knuckles;
 use ms_motion_controller::HolographicController;
-use hp_motion_controller::ReverbG2Controller;
 use oculus_touch::Touch;
 use openxr as xr;
 use simple_controller::SimpleController;
@@ -115,10 +115,8 @@ impl Profiles {
     pub fn get() -> &'static Self {
         // Add supported interaction profiles here.
         static P: Profiles = Profiles {
-            list: &[                (
-                    ControllerType::HPMotionController,
-                    &ReverbG2Controller,
-                ),
+            list: &[
+                (ControllerType::HPMotionController, &ReverbG2Controller),
                 (
                     ControllerType::SamsungOdysseyController,
                     &SamsungOdysseyController,
