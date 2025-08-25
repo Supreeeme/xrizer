1603945c599ec83ae2db2ae8a3d4b94d84d9a3cb
Mats Huesmann <huesmann.mats@gmail.com>
2025-08-25 21:56:40 +0200

    Add new BoundPoseType variants and update pose parsing logic

--- src/input/action_manifest.rs
+++ src/input/action_manifest.rs
@@ -570,11 +570,16 @@ fn parse_pose_binding<'de, D: serde::Deserializer<'de>>(
         }
     };
 
+    // See default BoundPoseType here: https://github.com/ValveSoftware/openvr/wiki/Render-Model-Reference
     let pose = match pose {
         "raw" => BoundPoseType::Raw,
         "tip" => BoundPoseType::Tip,
+        "base" => BoundPoseType::Base,
         "gdc2015" => BoundPoseType::Gdc2015,
-        other => return Err(D::Error::unknown_variant(other, &["raw", "tip", "gdc2015"])),
+        "grip" => BoundPoseType::Grip,
+        "handgrip" => BoundPoseType::Handgrip,
+        "openxr_handmodel" => BoundPoseType::OpenXRHandmodel,
+        other => return Err(D::Error::unknown_variant(other, &["raw", "tip", "base", "gdc2015", "handgrip", "openxr_handmodel"])),
     };
 
     Ok((hand, pose))
