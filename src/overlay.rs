ee317b1429b8038b830a94d742fe2f8ba2e0db61
Mats Huesmann <huesmann.mats@gmail.com>
2025-09-05 02:38:26 +0200

    Stub SetOverlayPreCurvePitch

--- src/overlay.rs
+++ src/overlay.rs
@@ -1148,7 +1148,8 @@ impl vr::IVROverlay028_Interface for OverlayMan {
         todo!()
     }
     fn SetOverlayPreCurvePitch(&self, _: vr::VROverlayHandle_t, _: f32) -> vr::EVROverlayError {
-        todo!()
+        crate::warn_unimplemented!("SetOverlayPreCurvePitch");
+        vr::EVROverlayError::None
     }
     fn GetOverlayCurvature(
         &self,
