ee0dd60e6b761b9e00f0ec2799c1006cd6d53ff7
hare_ware <3469405-hare_ware@users.noreply.gitlab.com>
2026-03-21 09:09:55 -0400

    Removed redundant static lifetimes

--- src/input/profiles/wmr/mod.rs
+++ src/input/profiles/wmr/mod.rs
@@ -8,11 +8,11 @@ pub mod hp_motion_controller;
 pub mod ms_motion_controller;
 pub mod samsung_odyssey_controller;
 
-const TRACKING_SYSTEM_NAME: &'static CStr = c"holographic";
-const SERIAL_NUMBER: Property<&'static CStr> = Property::PerHand {
+const TRACKING_SYSTEM_NAME: &CStr = c"holographic";
+const SERIAL_NUMBER: Property<&CStr> = Property::PerHand {
     left: c"MRSOURCE0",
     right: c"MRSOURCE1",
 };
-const OG_OPENVR_CONTROLLER_TYPE: &'static CStr = c"holographic_controller";
+const OG_OPENVR_CONTROLLER_TYPE: &CStr = c"holographic_controller";
 const OG_LEGACY_BUTTONS_MASK: u64 =
     button_mask_from_ids!(System, ApplicationMenu, Grip, Axis0, Axis1, Axis2, A);
